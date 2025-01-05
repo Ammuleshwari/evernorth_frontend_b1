fc055299641344b0b1671cc1ad26544a92d1f274
Ammuleshwari <reddyammuleshwari@gmail.com>
2025-01-05 23:23:18 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "\evernorth-profile",
   server: {
     historyApiFallback: true
   }
